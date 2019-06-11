50473b172febf362f8e99ceb7c48b62709506f47
Brendan Magee <bmagee@gmail.com>
2019-06-10 23:41:18 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This app is running in a Docker container
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn more about React
         </a>
       </header>
     </div>
